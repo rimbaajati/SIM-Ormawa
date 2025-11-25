c7af67f64349b17a2480a5b8002f38faa4605186
rimbaajati <audysa040802@gmail.com>
2025-11-25 22:41:03 +0700

    ApiCotroller

---
+++ backend/app/Http/Controllers/AdminController.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use App\Models\Proposal;
+
+class AdminController extends Controller
+{
+    public function index(){
+      $proposals = Proposal::with('user')->get();
+
+      return view('admin.index', compact('proposal'));
+    }
+}
