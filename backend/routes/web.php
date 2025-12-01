c912a883edcd093206c0b125841578935de0d953
rimbaajati <audysa040802@gmail.com>
2025-12-01 09:46:20 +0700

    add profile page

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -7,14 +7,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () { return view('layouts.index'); })->name('dashboard');
 
 //Manager Routes
-Route::middleware(['auth'])->group(function () {
-    Route::get('/manager/dashboard')
-        ->name('manager.dashboard');
-});
-// Route::get('/manager/dashboard', function () { return view('pages.manager.manager_dashboard'); })->name('manager_dashboard');
+Route::get('/manager/dashboard', function () { return view('pages.manager.manager_dashboard'); })->name('manager.dashboard');
 Route::get('/manager/allproposals',function(){ return view('pages.manager.manager_allproposal');})->name('manager.proposals.all');
 Route::get('/manager/allorganizations', function() { return view('pages.manager.manager_allorganization');})->name('manager.organization.all');
+Route::get('manager/allorganizations/organizationsprofile', function() { return view ('pages.manager.manager_organizationsprofile');})->name('manager.organizations.profile');
 Route::get('/manager/allschedules', function() { return view('pages.manager.manager_allschedule');})->name('manager.schedules.all');
+Route::get('/manager/profile', function() { return view('pages.manager.manager_profile');})->name('manager.profile');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
