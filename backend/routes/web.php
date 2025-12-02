759cdbb17ad84532db9025791825bb1db3eb7396
Ammar <01202207009@students.itspku.ac.id>
2025-12-02 16:17:10 +0700

    Update route login

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,33 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::middleware(['auth', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', fn() => 'ADMIN AREA');
-});
-
-Route::middleware(['auth', 'role:manager'])->group(function () {
-    Route::get('/manager/dashboard', fn() => 'MANAGER AREA');
-});
-
-Route::middleware(['auth', 'role:user'])->group(function () {
-    Route::get('/user/dashboard', fn() => 'USER AREA');
-});
-
 
 require __DIR__.'/auth.php';
