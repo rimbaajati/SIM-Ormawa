c1946147abb86172b587de2ea35faad6ed21ea6f
Ammar <01202207009@students.itspku.ac.id>
2025-12-02 14:55:24 +0700

    log res

---
+++ frontend/app/pages/login.vue
@@ -0,0 +1,7 @@
+<script setup>
+navigateTo("http://127.0.0.1:8000/login", { external: true });
+</script>
+
+<template>
+  <div></div>
+</template>
---
+++ frontend/app/pages/register.vue
@@ -0,0 +1,8 @@
+<script setup>
+navigateTo('http://127.0.0.1:8000/register', { external: true })
+</script>
+
+<template>
+  <div></div>
+</template>
+    
