52cd71cfe2be35aacc7863492a0b3830ebb94182
rimbaajati <audysa040802@gmail.com>
2025-12-04 09:08:13 +0700

    Update WEB.PHP

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,7 +5,7 @@ use App\Models\Proposal;
 use App\Http\Controllers\ProposalController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-
+  
 Route::get('/', function () {
     return view('welcome');
 });
